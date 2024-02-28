9d8d8a9addf0aeec6ad73ec2208a05cf6a2c0368
ajs656 <ajs656@tux4.cci.drexel.edu>
2024-02-28 18:27:51 -0500

    Added slider to form and added counter display for slider value

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ image_input.addEventListener("change", function() {
     reader.readAsDataURL(this.files[0]);
     alert("Image uploaded successfully");
 })
+
+var slider = document.getElementById("calorie-slider");
+var output = document.getElementById("calorie-count");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+} 
