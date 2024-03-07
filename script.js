1181001533c08fd5d7902dab68a40fcb8453e113
ajs656 <ajs656@tux5.cci.drexel.edu>
2024-03-06 19:10:33 -0500

    Added script.js for displaying list

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-const image_input = document.querySelector("#image_input");
-var uploaded_image = "";
-image_input.addEventListener("change", function() {
-    const reader  = new FileReader();
-    reader.addEventListener("load", () =>{
-        uploaded_image = reader.result;
-        document.querySelector("#image_preview").style.backgroundImage = `url(${uploaded_image})`
-        $("#spnMessage").html("Image Uploaded");
+var calorieSlider = document.getElementById("calorie-slider");
+var calorieCount = document.getElementById("calorie-count");
+calorieCount.innerHTML = calorieSlider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+calorieSlider.oninput = function () {
+    calorieCount.innerHTML = this.value;
+}
+var timeSlider = document.getElementById("timelimit");
+var timeLimitCount = document.getElementById("time-limit-count");
+timeLimitCount.innerHTML = timeSlider.value; // Display the default slider value
+// Update the current slider value (each time you drag the slider handle)
+timeSlider.oninput = function () {
+    timeLimitCount.innerHTML = this.value;
+}
+
+const listContainer = document.getElementById('list-container');
+
+const ingredients = [
+    ["Apples", 2, "lbs"],
+    ["Bananas", 3, "pcs"],
+    ["Milk", 1, "gal"],
+];
+
+function createListItem(ingredient, quantity, unit) {
+    const listItem = document.createElement('div');
+    listItem.classList.add('list-item');
+
+    const ingredientElement = document.createElement('span');
+    ingredientElement.classList.add('ingredient');
+    ingredientElement.textContent = ingredient;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-btn');
+    deleteButton.textContent = '❌';
+
+    listItem.appendChild(ingredientElement);
+    listItem.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', () => {
+        listContainer.removeChild(listItem);
     });
-    reader.readAsDataURL(this.files[0]);
-    alert("Image uploaded successfully");
-})
+    return listItem;
+}
+
+ingredients.forEach(ingredient => {
+    const listItem = createListItem(...ingredient);
+    listContainer.appendChild(listItem);
+});
+
